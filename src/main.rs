de077734388480b38320085eaab2f12b9f04c18f
= <=>
2025-04-08 14:48:52 +0530

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 #[allow(unused_imports)]
 use std::net::{TcpListener, TcpStream};
-use std::io::{Error as E, Read, Write, BufReader};
+use std::io::{Read, Write, BufReader};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").expect("Failed to bind to the addr");
-    //
+    let addr = listener.local_addr().expect("could't read the ip address");
     for stream in listener.incoming() {
          match stream {
              Ok(_stream) => {
@@ -23,14 +21,23 @@ fn main() {
 }
 
 fn handle_request(mut stream: TcpStream){
+    let addr = stream.local_addr().expect("could't read the ip address");
     let mut reader = BufReader::new(&stream);
     let mut buf: [u8; 1024] = [0; 1024];
 
     let bytes_read = reader.read(&mut buf).expect("Failed to read data from the input buffer");
     if bytes_read == 0{
         return;
-    }
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    };
 
-    let _ = stream.write_all(response.as_bytes()).expect("Failed to write to the buffer");
+    let request = String::from_utf8_lossy(&buf[..bytes_read]);
+    let split_request: Vec<&str> = request.split("/").collect();
+    let path = split_request[1];
+
+    let response = match path {
+        " HTTP" =>  "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n"
+    };
+
+    stream.write_all(response.as_bytes()).expect("Error occured while writing to buffer");
 }
