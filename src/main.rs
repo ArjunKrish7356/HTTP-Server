9a88e5bdc4f713b035211d2ca4cbfc601ef20610
= <=>
2025-04-08 09:21:54 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Error as E, Read, Write};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,11 +13,23 @@ fn main() {
     for stream in listener.incoming() {
          match stream {
              Ok(_stream) => {
-                 println!("accepted new connection");
+                 handle_request(_stream);
              }
              Err(e) => {
                  println!("error: {}", e);
              }
          }
      }
+}
+
+fn handle_request(mut stream: TcpStream){
+    let mut buf: [u8; 1024] = [0; 1024];
+
+    let _ = stream.read(&mut buf);
+    if buf.len() == 0{
+        return;
+    }
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    let _ = stream.write_all(response.as_bytes());
 }
